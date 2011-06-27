6e05a2cebc11de0f904bc2ef95282f80a7cae121
Cameron Higby-Naquin <cameron.higbynaquin@gmail.com>
2011-06-27 16:03:46 -0400

    Quickly indent/unindent.

--- keys.el
+++ keys.el
@@ -16,6 +16,9 @@
 
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
+(global-set-key (kbd "\C-c>") 'indent-region)
+(global-set-key (kbd "\C-c<") 'unindent-region)
+
 (global-set-key (kbd "M-s") 'isearch-forward-regexp)  ; was unbound
 (global-set-key (kbd "M-r") 'isearch-backward-regexp) ; was move-to-window-line
 
