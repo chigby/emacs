db4c281cf77e4045077e5dc72153ad5599b57741
Cameron Higby-Naquin <cameron.higbynaquin@gmail.com>
2011-11-22 22:01:12 -0500

    Use correct snippet dirs.

--- modes.el
+++ modes.el
@@ -257,7 +257,8 @@ it)"
 
 (require 'yasnippet)
 (yas/initialize)
-(yas/load-directory "~/.emacs.d/vendor/yasnippet/yasnippet-0.6.1c/snippets")
+(yas/load-directory "~/.emacs.d/vendor/yasnippet/snippets")
+(yas/load-directory "~/.emacs.d/snippets")
 
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'reverse)
