670fd4b4814e0f1a31a986d33d4e0f0359e71d15
Cameron Higby-Naquin <cameron.higbynaquin@gmail.com>
2013-05-23 17:35:35 -0400

    Cleanup whitespace and comments.

--- modes.el
+++ modes.el
@@ -155,9 +155,7 @@ it)"
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Puppet
 
-;; (autoload 'puppet-mode "puppet-mode" "Major mode for editing puppet manifests")
 (add-to-list 'auto-mode-alist '("\\.pp$" . puppet-mode))
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Misc.
