e75e30e7b2d0086fe0f936be50eb9f3fafef835f
Cameron Higby-Naquin <cameron.higbynaquin@gmail.com>
2014-02-11 18:37:27 -0500

    Remove unused function.
    Replaced by emacs 24 package functionality.

--- functions.el
+++ functions.el
@@ -71,27 +71,6 @@
 
  (global-set-key (kbd "C-c +") 'increment-number-at-point)
 
-;; For loading libraries from the vendor directory
-;; Modified from defunkt's original version to support autoloading.
-;; http://github.com/defunkt/emacs/blob/master/defunkt/defuns.el
-(defun vendor (library &rest autoload-functions)
-  (let* ((file (symbol-name library))
-         (normal (concat "~/.emacs.d/vendor/" file))
-         (suffix (concat normal ".el"))
-         (personal (concat "~/.emacs.d/" file))
-	 (found nil))
-    (cond
-     ((file-directory-p normal) (add-to-list 'load-path normal) (set 'found t))
-     ((file-directory-p suffix) (add-to-list 'load-path suffix) (set 'found t))
-     ((file-exists-p suffix)  (set 'found t)))
-    (when found
-      (if autoload-functions
-          (dolist (autoload-function autoload-functions)
-            (autoload autoload-function (symbol-name library) nil t))
-        (require library)))
-    (when (file-exists-p (concat personal ".el"))
-      (load personal))))
-
 ;; Macro to apply a fill to a specific line while leaving adjacent
 ;; lines untouched.
 (fset 'inline-fill
