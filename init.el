50af90b146c640206f7a16d04facecb1dcaf2921
Cameron Higby-Naquin <cameron.higbynaquin@gmail.com>
2014-05-12 17:20:29 -0400

    Remove any non-base python modes.
    Good riddance, so many unnecessary, out-of-date modes!

--- init.el
+++ init.el
@@ -34,7 +34,7 @@
 
 (require 'package)
 
-(setq package-list '(ack-and-a-half auctex puppet-mode apache-mode dsvn whitespace php-mode markdown-mode python js2-mode yasnippet git-commit yaml-mode))
+(setq package-list '(ack-and-a-half auctex puppet-mode apache-mode dsvn whitespace php-mode markdown-mode js2-mode yasnippet git-commit yaml-mode rvm))
 
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
