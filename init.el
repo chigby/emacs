38431658e3a1b44d06854b57c27e40c33325ef9a
Cameron Higby-Naquin <cameron.higbynaquin@gmail.com>
2014-05-14 23:51:40 -0400

    Clean-up customize-set-variables/faces.
    Remove unused stuff, whitespace adjustments, etc.

--- init.el
+++ init.el
@@ -82,23 +82,15 @@
 ;;; Custom stuff
 
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(column-number-mode t)
  '(paren-match-face (quote paren-face-match-light))
  '(paren-sexp-mode t)
  '(rst-level-face-base-light 50)
- '(show-paren-mode t)
- '(tab-width 8)
+ '(show-paren-mode t nil (paren))
  '(transient-mark-mode t))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(flymake-errline ((((class color)) (:background "zenburn-bg" :foreground "#ef8500"))))
- '(flymake-warnline ((((class color)) (:background "zenburn-bg" :foreground "#93e0e3")))))
 
 (eshell)
