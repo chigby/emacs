b97a09fb1c0beae684993974d9493b9a2f0e044a
Cameron Higby-Naquin <me@chigby.org>
2016-04-08 13:41:00 -0400

    Add useful command to edit quickly via sudo.

--- lisp/functions.el
+++ lisp/functions.el
@@ -230,3 +230,9 @@
           (set-buffer-modified-p nil)
           (message "File '%s' successfully renamed to '%s'"
                    name (file-name-nondirectory new-name)))))))
+
+(defun sudo-edit (&optional arg)
+  (interactive "p")
+  (if (or arg (not buffer-file-name))
+      (find-file (concat "/sudo:root@localhost:" (ido-read-file-name "File: ")))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
