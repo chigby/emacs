345ef5b787a03ecc26cf87981f2969948f47c0b1
Cameron Higby-Naquin <me@chigby.org>
2018-07-23 10:39:13 -0400

    Attempt to speed up magit & VCS use in general

--- lisp/chn-git.el
+++ lisp/chn-git.el
@@ -4,6 +4,12 @@
 (package-require 'magit)
 (global-set-key (kbd "C-x g") 'magit-status)
 
+(setq magit-auto-revert-mode nil)
+
 (defun turn-off-ethan-wspace ()
   (ethan-wspace-mode -1))
 (add-hook 'git-commit-mode-hook 'turn-off-ethan-wspace)
+
+;; We're not using vc for anything, and it's enabled by default, so
+;; turn it off.
+(setq vc-handled-backends nil)
