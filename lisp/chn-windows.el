d27b541971a583da06635f5268fa1a735c4f7cfc
Cameron Higby-Naquin <me@chigby.org>
2019-05-11 18:26:34 -0400

    windows: Some enhancements

--- lisp/chn-windows.el
+++ lisp/chn-windows.el
@@ -1,8 +1,19 @@
 (set-default-font "Consolas 14")
 
+(use-package powershell)
+(use-package ahk-mode)
+
 (defun run-powershell ()
   "Run powershell"
   (interactive)
   (async-shell-command "c:/windows/system32/WindowsPowerShell/v1.0/powershell.exe -Command -"
+                       nil
+                       nil))
+
+
+(defun run-powershell2 ()
+  "Run powershell"
+  (interactive)
+  (async-shell-command "C:/Program Files/PowerShell/6.0.3/pwsh.exe -Command -"
                        nil
                        nil))
