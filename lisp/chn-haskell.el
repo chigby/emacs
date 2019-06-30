fa474dc0e370b367623fc5f4df627ebb3f0d47f5
Cameron Higby-Naquin <me@chigby.org>
2019-06-30 10:18:01 -0400

    haskell: Better indentation

--- lisp/chn-haskell.el
+++ lisp/chn-haskell.el
@@ -1,7 +1,15 @@
 ;;; chn-haskell.el --- "We must use this because Hask is not a category"
 
+(defun haskell-setup ()
+  "Setup variables for editing Haskell files."
+  (setq whitespace-line-column 70)
+  (make-local-variable 'tab-stop-list)
+  (setq tab-stop-list (number-sequence 2 80 2))
+  (haskell-indentation-mode 0)
+  (setq indent-line-function 'indent-relative))
+
 (use-package haskell-mode
   :config
-  (add-hook 'haskell-mode-hook 'turn-on-haskell-indent))
+  (add-hook 'haskell-mode-hook 'haskell-setup))
 
 (provide 'chn-haskell)
