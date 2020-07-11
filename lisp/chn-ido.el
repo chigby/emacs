ad35581efa79406466d057edf9685f7d91c91606
Cameron Higby-Naquin <me@chigby.org>
2020-07-11 11:52:53 -0400

    Ensure ido-mode uses is case insensitive

--- lisp/chn-ido.el
+++ lisp/chn-ido.el
@@ -2,6 +2,7 @@
 
 (ido-mode t)
 (setq ido-enable-flex-matching t ;; fuzzy matching
+      ido-case-fold t
       ido-use-virtual-buffers t) ;; Find past buffers as well as existing ones
 
 (provide 'chn-ido)
