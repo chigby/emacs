53b614ebc9b61fbea6bdd33d0a6c1c8bb2d8e12b
Cameron Higby-Naquin <me@chigby.org>
2021-03-14 10:32:39 -0400

    Run test commands asynchronously
    This is just faster and does't block emacs for like ~10 seconds during
    long test runs (which, like, I don't really approve of either, but so
    it goes).

--- lisp/chn-lib.el
+++ lisp/chn-lib.el
@@ -16,13 +16,16 @@
 
 (defun ubuntu-shell-command (command)
   "Execute a shell command in the Windows Subsystem for Linux environment."
-  (shell-command (format "C:\\windows\\system32\\wsl.exe bash -ic \"%s\"" command)))
+  (async-shell-command (format "C:\\windows\\system32\\wsl.exe bash -ic \"%s\"" command)))
 
+;; maybe try this?
+;; https://emacs.stackexchange.com/questions/8276/run-async-shell-command-in-view-mode
+;; can we run these commands and have the resulting buffer be in view-mode?
 (defun execute-bash-script (filename)
   "Cross platform bash commands (requires ubuntu on windows)."
   (cond
    ((eq system-type 'gnu/linux)
-    (shell-command filename))
+    (async-shell-command filename))
    ((eq system-type 'windows-nt)
     (ubuntu-shell-command filename))
   ))
--- lisp/chn-window-nav.el
+++ lisp/chn-window-nav.el
@@ -26,6 +26,7 @@
 (add-to-list 'display-buffer-alist '("\\*elm-make\\*" chn-temp-window))
 (add-to-list 'display-buffer-alist '("\\*rg\\*" chn-temp-window))
 (add-to-list 'display-buffer-alist '("\\*Occur\\*" chn-temp-window))
+(add-to-list 'display-buffer-alist '("\\*Async Shell Command\\*" chn-temp-window))
 
 (setq help-window-select t)
 
