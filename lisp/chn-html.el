27792a08ed1f99553efc34c0ea912ebe662eb3ba
Cameron Higby-Naquin <me@chigby.org>
2022-03-12 11:55:54 -0500

    sass: Fix some problems that I was having
    Source: https://stackoverflow.com/a/21361009/1382355
    
    Rolling my eyes a little here.  No idea what any of this stuff does
    but it did fix "some problems" as described.
    
    Another thing that might be good here: turning off electric indent
    mode while in Sass mode.

--- lisp/chn-html.el
+++ lisp/chn-html.el
@@ -32,4 +32,35 @@
 (defun chn/check-django ()
   (when (in-project-root? "manage.py") (web-mode-set-engine "django")))
 
+(use-package sass-mode
+  :mode (("\\.sass\\'" . sass-mode)))
+
+(defconst sass-line-keywords
+  '(("@\\(\\w+\\)"    0 font-lock-keyword-face sass-highlight-directive)
+("/[/*].*"  0 font-lock-comment-face)
+("[=+]\\w+" 0 font-lock-variable-name-face sass-highlight-script-after-match)
+("!\\w+"    0 font-lock-variable-name-face sass-highlight-script-after-match)
+(":\\w+"    0 font-lock-variable-name-face)
+("\\w+\s*:" 0 font-lock-variable-name-face)
+("\\(\\w+\\)\s*="  1 font-lock-variable-name-face sass-highlight-script-after-match)
+("\\(:\\w+\\)\s*=" 1 font-lock-variable-name-face sass-highlight-script-after-match)
+(".*"      sass-highlight-selector)))
+
+(defconst sass-selector-font-lock-keywords
+  '( ;; Attribute selectors (e.g. p[foo=bar])
+("\\[\\([^]=]+\\)" (1 font-lock-variable-name-face)
+ ("[~|$^*]?=\\([^]=]+\\)" nil nil (1 font-lock-string-face)))
+("&"       0 font-lock-constant-face)
+("\\.\\w+" 0 font-lock-type-face)
+("#\\w+"   0 font-lock-keyword-face)
+;; Pseudo-selectors, optionally with arguments (e.g. :first, :nth-child(12))
+("\\(::?\\w+\\)" (1 font-lock-variable-name-face)
+ ("(\\([^)]+\\))" nil nil (1 font-lock-string-face)))))
+(defconst sass-non-block-openers
+  '("^.*,$" ;; Continued selectors
+"^ *@\\(extend\\|debug\\|warn\\|include\\|import\\)" ;; Single-line mixins
+"^ *[$!]"     ;; Variables
+".*[^\s-]+: [^\s-]" ;; a setting of some sort
+))
+
 (provide 'chn-html)
