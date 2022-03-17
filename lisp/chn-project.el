7881ab27b4d5ee18f5c474a08c18659d53e39456
Cameron Higby-Naquin <me@chigby.org>
2022-03-17 18:43:33 -0400

    Replace headlong/bookmarks with project.el
    Project.el is built-in to emacs, but we're installing the Gnu ELPA
    package to get features from emacs 28.
    
    It's not quite the same experience, but both of these pieces of code
    are designed for the same purpose.  Still need some fine tuning to
    preserve the speed of the old technique, while also having the
    advantages of the newer one (immediately activating the customizable
    command menu, and then subsequently having access to the `C-x p` set
    of commands.

--- lisp/chn-project.el
+++ lisp/chn-project.el
@@ -12,9 +12,38 @@
 (bookmark-maybe-load-default-file)
 (global-set-key (kbd "C-<f6>") 'bookmark-bmenu-list)
 
-;; refer to https://oremacs.com/2015/01/06/rushing-headlong/
-(use-package headlong
-  :bind ("M-;" . headlong-bookmark-jump))
+;; Headlong -- deprecated
+;; ;; refer to https://oremacs.com/2015/01/06/rushing-headlong/
+;; (use-package headlong
+;;   :bind ("M-;" . headlong-bookmark-jump))
+;; consider alternatives: https://www.reddit.com/r/emacs/comments/q1zi0t/package_shoutout_consultdir_and_embark/
+;; consult-dir slash embark?
+;; see also https://protesilaos.com/emacs/dotemacs section 3.1.5.1 (switch to directories)
+;; also relevant (but not as a headlong replacement) is section 3.1.7 of the above document
+;; more about project.el: https://www.reddit.com/r/emacs/comments/kfubcb/comment/ggav4un/
+
+(use-package project
+  :custom
+  (project-switch-commands
+   '((?f "File" project-find-file)
+     (?d "Dired" project-dired)
+     (?r "Ripgrep" rg-project)
+     (?m "Magit" magit-project-status)
+     (?b "Buffer" project-switch-to-buffer)
+     ))
+  :bind ("M-;" . project-switch-project))
+
+;; some next ideas:
+;;
+;; 1. figure out how to used `fd' instead of `find' for
+;; `project-files'.  See
+;; https://www.manueluberti.eu/emacs/2020/11/14/extending-project/ for
+;; more information.
+;; 2. Consider binding C-c f (or C-c C-f) to project-find-file --
+;; though I'm also okay to ponder the default keybinding for a while
+;; to see if, I dunno, it builds my character (and loops me into the
+;; rest of the commands that C-x p prefixes, maybe).
+
 
 (use-package dumb-jump
   :config
