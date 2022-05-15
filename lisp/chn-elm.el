6275de7ed681396e31e8a6d5967adaad5462b3a6
Cameron Higby-Naquin <me@chigby.org>
2022-05-15 16:34:40 -0400

    Ensure we're using the "working" elm indent mode
    The simple mode doesn't really act how I want, and without this, I
    feel like the file gets put into "no mode" and then it falls back to
    the default indentation mode, which is even worse!

--- lisp/chn-elm.el
+++ lisp/chn-elm.el
@@ -4,6 +4,8 @@
   :config
   (setq elm-format-on-save t)
   (bind-key "C-c C-c" 'elm-compile-main elm-mode-map)
+  :hook
+  (elm-mode . elm-indent-mode)
   :custom
   (elm-package-json "elm.json" "Support elm 0.19"))
 
