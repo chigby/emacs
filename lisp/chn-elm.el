00d4428eb09fd402f45b8b04000cbd29ba191d5d
Cameron Higby-Naquin <me@chigby.org>
2025-04-18 15:44:38 -0400

    Smooth over elm config

--- lisp/chn-elm.el
+++ lisp/chn-elm.el
@@ -1,13 +1,13 @@
 ;;; chn-elm.el --- Elm Language Support
 
 (use-package elm-mode
-  :defer t
-  :config
-  (setq elm-format-on-save t)
-  (bind-key "C-c C-c" 'elm-compile-main elm-mode-map)
+  :bind
+  (:map elm-mode-map
+        ("C-c C-c" . elm-compile-main))
   :hook
   (elm-mode . elm-indent-mode)
   :custom
+  (elm-format-on-save t)
   (elm-package-json "elm.json" "Support elm 0.19"))
 
 
