faa3ce933b508dcd5f0486bb392c6e534cf2ee4b
Cameron Higby-Naquin <me@chigby.org>
2026-08-10 16:09:04 -0400

    Better shell/project integration

--- lisp/chn-eshell.el
+++ lisp/chn-eshell.el
@@ -9,5 +9,30 @@
           (lambda ()
             (company-mode 0)))
 
+(defun chn/setup-shell ()
+  (setq-local comint-prompt-read-only t)
+  (setq-local comint-process-echoes t)
+  (setq show-trailing-whitespace nil)
+
+  ;; Remove the "kill running process?" query on exit.
+  (let ((proc (get-buffer-process (current-buffer))))
+   (when (processp proc)
+     (set-process-query-on-exit-flag proc nil))))
+
+(use-package coterm
+  :ensure t
+  :config (coterm-mode))
+
+(use-package shell
+  :ensure nil ;; built-in library
+  :after (consult)
+  :commands shell
+  :bind (
+         ("C-x C-z" . shell)
+         :map shell-mode-map
+         ([remap comint-history-isearch-backward-regexp] . consult-history)
+         )
+  :hook
+  (shell-mode . chn/setup-shell))
 
 (provide 'chn-eshell)
