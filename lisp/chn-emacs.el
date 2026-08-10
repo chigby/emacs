a334d84c368e34d28b38a5771d14ac58c400799e
Cameron Higby-Naquin <me@chigby.org>
2026-08-10 16:03:22 -0400

    Add more embark/corfu config

--- lisp/chn-emacs.el
+++ lisp/chn-emacs.el
@@ -6,6 +6,22 @@
   :custom
   (confirm-kill-emacs #'yes-or-no-p)
   (ring-bell-function (lambda () (message "*beep*")))
+
+  ;; Completion configuration (via Corfu)
+  ;; TAB cycle if there are only few candidates
+  (completion-cycle-threshold 3)
+
+  ;; Enable indentation+completion using the TAB key.
+  ;; `completion-at-point' is often bound to M-TAB.
+  (tab-always-indent 'complete)
+
+  ;; Disable Ispell completion function.
+  (text-mode-ispell-word-completion nil)
+  ;; Hide commands in M-x which do not apply to the current mode.  Corfu
+  ;; commands are hidden, since they are not used via M-x. This setting is
+  ;; useful beyond Corfu.
+  (read-extended-command-predicate #'command-completion-default-include-p)
+
   :preface
   (defun chn/display-ansi-codes (buf &rest _)
       (and (bufferp buf)
