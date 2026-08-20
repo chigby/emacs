dae67427658e204eecfcdcc738472d58d2cfc740
Cameron Higby-Naquin <me@chigby.org>
2026-08-20 18:10:17 -0400

    Remove deprecated `increment-number-at-point`

--- lisp/chn-functions.el
+++ lisp/chn-functions.el
@@ -33,24 +33,6 @@
                             (backward-char 1)))
                      ))))))
 
-(defun increment-number-at-point (&optional arg)
-  "Increment the number forward from point by 'arg'."
-  (interactive "p*")
-  (save-excursion
-    (save-match-data
-      (let (inc-by field-width answer)
-        (setq inc-by (if arg arg 1))
-        (skip-chars-backward "0123456789")
-        (when (re-search-forward "[0-9]+" nil t)
-          (setq field-width (- (match-end 0) (match-beginning 0)))
-          (setq answer (+ (string-to-number (match-string 0) 10) inc-by))
-          (when (< answer 0)
-            (setq answer (+ (expt 10 field-width) answer)))
-          (replace-match (format (concat "%0" (int-to-string field-width) "d")
-                                 answer)))))))
-
- (global-set-key (kbd "C-c +") 'increment-number-at-point)
-
 ;; Macro to apply a fill to a specific line while leaving adjacent
 ;; lines untouched.
 (fset 'inline-fill
