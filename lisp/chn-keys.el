74bc0b8998a582b2ff4f0915336836f0114511fb
Cameron Higby-Naquin <me@chigby.org>
2026-09-18 15:36:32 -0400

    Remove `M-r` shortcut for `C-M-r`
    I am so rarely typing this.

--- lisp/chn-keys.el
+++ lisp/chn-keys.el
@@ -1,8 +1,6 @@
 (global-set-key (kbd "\C-c>") 'indent-region)
 (global-set-key (kbd "\C-c<") 'unindent-region)
 
-(global-set-key (kbd "M-r") 'isearch-backward-regexp) ; was move-to-window-line
-
 (global-set-key [f5] 'kmacro-start-macro-or-insert-counter)  ;; use C-x e to end macro
 (global-set-key [f7] 'revert-buffer)
 
