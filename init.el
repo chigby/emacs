673bd74c6b057e3acd01ca6e6aa7fb49fdfd23bd
Cameron Higby-Naquin <me@chigby.org>
2026-09-26 11:17:50 -0400

    Move bespoke mode configs to init.el section

--- init.el
+++ init.el
@@ -301,6 +301,15 @@ Version 2016-04-04"
   :custom
   (ispell-program-name "hunspell"))
 
+;;;; Local packages for bespoke programming languages
+(use-package sugarcube-mode
+  :ensure nil ;; it's a local package, don't try to install it from a repo
+  :mode "\\.twee\\'")
+
+(use-package chn-octo-mode
+  :ensure nil
+  :mode "\\.octo\\'")
+
 ;;; Platform-specific code
 ;;;; Windows
 (use-package chn-windows
